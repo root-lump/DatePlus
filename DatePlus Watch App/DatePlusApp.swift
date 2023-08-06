23e1fbc27a87c79e1f9c4fa1df67e8715339699d
root-lump <root-hannmaburasuta25@hotmail.co.jp>
2023-08-07 02:30:11 +0900

    Initial Commit

---
+++ DatePlus Watch App/DatePlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlusApp.swift
+//  DatePlus Watch App
+//
+//  Created by √lump on 2023/08/07.
+//
+
+import SwiftUI
+
+@main
+struct DatePlus_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
